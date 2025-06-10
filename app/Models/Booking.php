cf0531fbfad6d9da4c6ee0a597f220ead7c609d8
Dean Noiron <noirondean@gmail.com>
2025-06-10 10:25:25 +0200

    booking model + correction trip model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,9 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Trip;
+use App\Traits\HasUuid;
 
 class Booking extends Model
 {
     /** @use HasFactory<\Database\Factories\BookingFactory> */
     use HasFactory;
+    use HasUuid;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'trip_id',
+        'name',
+        'email',
+        'number_of_people',
+        'status'
+    ];
+
+    
+    public function trips(){
+        return $this->belongsTo(Trips::class);
+    }
 }
--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -25,6 +25,7 @@ class Trip extends Model
     ];
 
     public function booking(){
-        return $this->belongsTo(Booking::class);
+        return $this->hasMany(Booking::class);
     }
+    
 }
