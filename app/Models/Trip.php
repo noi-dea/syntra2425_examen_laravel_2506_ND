2ed33d3125549d57b55d2d8eec40aa65a6cf1723
Dean Noiron <noirondean@gmail.com>
2025-06-10 10:20:28 +0200

    Trip Model

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Booking;
+use App\Traits\HasUuid;
 
 class Trip extends Model
 {
     /** @use HasFactory<\Database\Factories\TripFactory> */
     use HasFactory;
+    use HasUuid;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'title',
+        'region',
+        'start_date',
+        'duration_days',
+        'price_per_person'
+    ];
+
+    public function booking(){
+        return $this->belongsTo(Booking::class);
+    }
 }
