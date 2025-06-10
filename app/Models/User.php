7df5dde63da3d84673655225e56196eccd779d32
Dean Noiron <noirondean@gmail.com>
2025-06-10 09:58:18 +0200

    initializing + preparing for useage of uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Traits\HasUuid;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use HasUuid;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
