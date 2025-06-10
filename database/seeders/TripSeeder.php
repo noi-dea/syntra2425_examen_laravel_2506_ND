ccfb85e40f2eca3c675d017e9e23932537fd667b
Dean Noiron <noirondean@gmail.com>
2025-06-10 10:45:13 +0200

    trip seeder

--- database/seeders/TripSeeder.php
+++ database/seeders/TripSeeder.php
@@ -12,6 +12,46 @@ class TripSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $trips = [[
+            'title' => 'musea marathon',
+            'region' => 'west',
+            'start_date' => '2026-03-14',
+            'duration_days' => 2,
+            'price_per_person' => 42
+        ],[
+            'title' => 'Mountaineering',
+            'region' => 'east',
+            'start_date' => '2025-09-13',
+            'duration_days' => 5,
+            'price_per_person' => 156
+        ],[
+            'title' => 'skeeing',
+            'region' => 'north',
+            'start_date' => '2025-08-05',
+            'duration_days' => 3,
+            'price_per_person' => 93 
+        ],[
+            'title' => 'Historical city tour',
+            'region' => 'central',
+            'start_date' => '2025-10-27',
+            'duration_days' => 1,
+            'price_per_person' => 30 
+        ],[
+            'title' => 'snowboarding',
+            'region' => 'north',
+            'start_date' => '2026-07-15',
+            'duration_days' => 2,
+            'price_per_person' => 56 
+        ],[
+            'title' => 'New Years Hike',
+            'region' => 'west',
+            'start_date' => '2026-01-01',
+            'duration_days' => 3,
+            'price_per_person' => 78
+        ]];
+
+        foreach($trips as $trip){
+            Enemy::factory()->create($trip);
+        }
     }
 }
